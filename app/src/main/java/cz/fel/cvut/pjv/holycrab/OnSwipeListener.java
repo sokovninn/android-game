ba757cab2b60278626c5f6150a34ecb1feb86a66
sokovnik <sokovnik@fel.cvut.cz>
2019-04-26 23:25:34 +0200

    SwipeListener added

---
+++ app/src/main/java/cz/fel/cvut/pjv/holycrab/OnSwipeListener.java
@@ -0,0 +1,60 @@
+package cz.fel.cvut.pjv.holycrab;
+
+import android.graphics.Point;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+public class OnSwipeListener extends GestureDetector.SimpleOnGestureListener {
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+        float x1 = event1.getX();
+        float y1 = event1.getY();
+        float x2 = event2.getX();
+        float y2 = event2.getY();
+
+        Direction direction = getDirection(x1, y1, x2, y2);
+        return onSwipe(direction);
+    }
+
+    public boolean onSwipe (Direction direction) {
+        return false;
+    }
+
+    public Direction getDirection(float x1, float y1, float x2, float y2){
+        double angle = getAngle(x1, y1, x2, y2);
+        return Direction.fromAngle(angle);
+    }
+
+    public double getAngle(float x1, float y1, float x2, float y2) {
+
+        double rad = Math.atan2(y1-y2,x2-x1) + Math.PI;
+        return (rad*180/Math.PI + 180)%360;
+    }
+
+    public enum Direction{
+        up,
+        down,
+        left,
+        right;
+
+        public static Direction fromAngle(double angle){
+            if(inRange(angle, 45, 135)){
+                return Direction.up;
+            }
+            else if(inRange(angle, 0,45) || inRange(angle, 315, 360)){
+                return Direction.right;
+            }
+            else if(inRange(angle, 225, 315)){
+                return Direction.down;
+            }
+            else{
+                return Direction.left;
+            }
+
+        }
+
+        private static boolean inRange(double angle, float init, float end){
+            return (angle >= init) && (angle < end);
+        }
+    }
+}
