372eda55e1ab51b78b8f352821ed3178c55d602c
sokovnik <sokovnik@fel.cvut.cz>
2019-05-01 20:53:24 +0200

    Door types added

---
+++ app/src/main/java/cz/fel/cvut/pjv/holycrab/SteelDoor.java
@@ -0,0 +1,7 @@
+package cz.fel.cvut.pjv.holycrab;
+
+public class SteelDoor extends Door {
+    public SteelDoor(Location location, Room currentRoom, Room nextRoom) {
+        super(location, currentRoom, nextRoom);
+    }
+}
---
+++ app/src/main/java/cz/fel/cvut/pjv/holycrab/WoodenDoor.java
@@ -0,0 +1,7 @@
+package cz.fel.cvut.pjv.holycrab;
+
+public class WoodenDoor extends Door {
+    public WoodenDoor(Location location, Room currentRoom, Room nextRoom) {
+        super(location, currentRoom, nextRoom);
+    }
+}
